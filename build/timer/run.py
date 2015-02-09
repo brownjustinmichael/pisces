88f31068a6657c515af478d6cf57632cd564c632
Justin Brown <brown.justin.michael@gmail.com>
2015-02-09 13:39:31 -0800

    Minor changes to check that timing works

--- build/timer/run.py
+++ build/timer/run.py
@@ -9,9 +9,9 @@ timer = Timer ("pisces",
                Argument ("-V parallel.transform.threads %d", extent = [1,2], upperBound = maxThreads), 
                setupCommandRoot = "pisces_init", 
                directory = "../../run", 
-               commandArgs = ["-D7"], 
+               commandArgs = ["-D2"], 
                uniques = [Argument ("-V input.file input_%03d_%%02i")])
 
-results = timer.calculateTimes (torque = True)
+results = timer.calculateTimes (torque = True, iterations = 8)
 
 print (results)
