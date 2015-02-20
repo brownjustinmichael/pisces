424c220049f5e5a81556ba9e682bd2f85e141e08
Justin Brown <brown.justin.michael@gmail.com>
2015-02-19 17:20:32 -0800

    Fixed bug in advection that made velocity backwards and caused splitting of plumes at boundaries

--- src/plans/advection/advection.hpp
+++ src/plans/advection/advection.hpp
@@ -36,7 +36,7 @@ namespace plans
 	public:
 		advection (plans::grid <datatype> &i_grid_n, plans::grid <datatype> &i_grid_m, datatype i_coeff, datatype* i_vel_n, datatype *i_vel_m, datatype *i_data_in, datatype *i_data_out = NULL, int *i_element_flags = NULL, int *i_component_flags = NULL) :
 		real_plan <datatype> (i_grid_n, i_grid_m, i_data_in, i_data_out, i_element_flags, i_component_flags),
-		coeff (i_coeff),
+		coeff (-i_coeff),
 		vel_n (i_vel_n),
 		vel_m (i_vel_m),
 		pos_n (&(grid_n [0])),
@@ -51,11 +51,11 @@ namespace plans
 			oodz_vec.resize (m);
 			oodz_ptr = &oodz_vec [0];
 			
-			oodx_ptr [0] = 1.0 / (pos_n [1] - pos_n [0]);
+			oodx_ptr [0] = 0.5 / (pos_n [1] - pos_n [0]);
 			for (int i = 1; i < n - 1; ++i) {
 				oodx_ptr [i] = 1.0 / (pos_n [i + 1] - pos_n [i - 1]);
 			}
-			oodx_ptr [n - 1] = 1.0 / (pos_n [n - 1] - pos_n [n - 2]);
+			oodx_ptr [n - 1] = 0.5 / (pos_n [n - 1] - pos_n [n - 2]);
 			
 			oodz_ptr [0] = 1.0 / (pos_m [1] - pos_m [0]);
 			for (int i = 1; i < m - 1; ++i) {
@@ -67,21 +67,21 @@ namespace plans
 		virtual ~advection () {}
 	
 		virtual void execute () {
-			linalg::matrix_copy (m, n - 1, data_in, x_ptr + m);
-			linalg::matrix_add_scaled (m, n - 1, -1.0, data_in + m, x_ptr);
-			linalg::add_scaled (m, -1.0, data_in, x_ptr);
-			linalg::add_scaled (m, 1.0, data_in + m * (n - 1), x_ptr + m * (n - 1));
-
-			linalg::matrix_copy (m - 1, n, data_in, z_ptr + 1);
-			linalg::matrix_add_scaled (m - 1, n, -1.0, data_in + 1, z_ptr);
-			linalg::add_scaled (n, -1.0, data_in + m - 1, z_ptr, m, m);
-			linalg::add_scaled (n, 1.0, data_in, z_ptr + m - 1, m, m);
+			linalg::scale (m, 0.0, x_ptr + m * (n - 1));
+			linalg::matrix_copy (m, n - 1, data_in + m, x_ptr);
+			linalg::matrix_add_scaled (m, n - 1, -1.0, data_in, x_ptr + m);
+			linalg::add_scaled (m, 1.0, data_in, x_ptr + m * (n - 1));
+			linalg::add_scaled (m, -1.0, data_in + m * (n - 1), x_ptr);
+			
+			linalg::scale (n, 0.0, z_ptr + m - 1, m);
+			linalg::matrix_copy (m - 1, n, data_in + 1, z_ptr, m, m);
+			linalg::matrix_add_scaled (m - 1, n, -1.0, data_in, z_ptr + 1, m, m);
+			linalg::add_scaled (n, 1.0, data_in, z_ptr, m, m);
+			linalg::add_scaled (n, -1.0, data_in + m - 1, z_ptr + m - 1, m, m);
 			
 			#pragma omp parallel for
 			for (int j = 0; j < m; ++j) {
-				x_ptr [j] = 0.0;
-				x_ptr [(n - 1) * m + j] = 0.0;
-				for (int i = 1; i < n - 1; ++i) {
+				for (int i = 0; i < n; ++i) {
 					x_ptr [i * m + j] = (vel_n [i * m + j] * x_ptr [i * m + j] * oodx_ptr [i] + vel_m [i * m + j] * z_ptr [i * m + j] * oodz_ptr [j]);
 				}
 			}
