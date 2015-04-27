698e3e47cdb51b12b07ee32be306f261cc94cf23
Justin Brown <brown.justin.michael@gmail.com>
2015-04-27 13:46:28 -0700

    Change for Hyades compliance

--- src/plans/plans-source/source.hpp
+++ src/plans/plans-source/source.hpp
@@ -102,7 +102,7 @@ namespace plans
 					if (coeff) {
 						return std::shared_ptr <plans::plan <datatype> > (new uniform <datatype> (*grids [0], *grids [1], coeff, data_source, i_data_in, i_data_out, i_element_flags, i_component_flags));
 					}
-					return std::shared_ptr <plans::plan <datatype> > (NULL);
+					return std::shared_ptr <plans::plan <datatype> > ();
 				}
 			};
 		};
