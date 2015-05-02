cb9b6901f122f16ba6d41cbabb88e718ce73a65f
Justin Brown <brown.justin.michael@gmail.com>
2015-05-01 20:18:09 -0700

    Changed name from pisces to isces

--- build/timer/run.py
+++ build/timer/run.py
@@ -3,12 +3,12 @@ from timing import Timer, Argument
 mpiProcs = Argument ("mpirun -np %d", extent = [1, 2, 4, 8], prepend = True, processes = True, runOnly = "-machinefile $HOSTFILE")
 maxThreads = Argument ("-V parallel.maxthreads %d", extent = [1, 8], threads = True)
 
-timer = Timer ("pisces", 
+timer = Timer ("isces", 
                mpiProcs,
                maxThreads, 
                Argument ("-V parallel.transform.threads %d", extent = [1, 8], upperBound = maxThreads),
                # Argument ("-V grid.z.points %d", value = 256) * mpiProcs, 
-               setupCommandRoot = "pisces_init", 
+               setupCommandRoot = "isces_init", 
                directory = "../../run", 
                commandArgs = ["-D2"],
                uniques = [Argument ("-V input.file input_%03d_%%02i")])
