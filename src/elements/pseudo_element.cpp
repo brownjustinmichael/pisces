f731602577831710b7ae3461841432ad4449f927
Justin Brown <brown.justin.michael@gmail.com>
2015-07-05 13:45:16 -0600

    Made pisces solver, but it isn't working.

---
+++ src/elements/pseudo_element.cpp
@@ -0,0 +1,30 @@
+/*!**********************************************************************
+ * \file pseudo.cpp
+ * /Users/justinbrown/Dropbox/pisces
+ * 
+ * Created by Justin Brown on 2014-02-03.
+ * Copyright 2014 Justin Brown. All rights reserved.
+ ************************************************************************/
+
+#include "pseudo_element.hpp"
+
+#include "plans-solvers/solvers.hpp"
+
+namespace pisces
+{
+	using namespace plans;
+	
+	template <class datatype>
+	pseudo_element <datatype>::pseudo_element (grids::axis i_axis_n, grids::axis i_axis_m, int i_name, io::parameters& i_params, data::data <datatype> &i_data, mpi::messenger* i_messenger_ptr, int i_element_flags, bool load_diffusion) : 
+	boussinesq_element <datatype> (i_axis_n, i_axis_m, i_name, i_params, i_data, i_messenger_ptr, i_element_flags, load_diffusion) {
+
+		// Set up the velocity constraint
+		*pdiv <datatype> (equations ["pressure"], equations ["x_velocity"], equations ["z_velocity"])
+		==
+		0.0;
+		
+	TRACE ("Initialized.");
+	}
+
+	template class pseudo_element <double>;
+} /* pisces */
---
+++ src/elements/pseudo_element.hpp
@@ -0,0 +1,37 @@
+/*!**********************************************************************
+ * \file pseudo.hpp
+ * /Users/justinbrown/Dropbox/pisces
+ * 
+ * Created by Justin Brown on 2014-02-03.
+ * Copyright 2014 Justin Brown. All rights reserved.
+ ************************************************************************/
+
+#ifndef pseudo_TWO_D_HPP_OQ800X4X
+#define pseudo_TWO_D_HPP_OQ800X4X
+
+#include "boussinesq.hpp"
+
+namespace pisces
+{
+	/*!**********************************************************************
+	 * \brief An element built to use the pseudo approximation
+	 ************************************************************************/
+	template <class datatype>
+	class pseudo_element : public boussinesq_element <datatype>
+	{	
+	protected:
+		using boussinesq_element <datatype>::equations;
+
+	public:
+		using element <datatype>::ptr;
+		
+		/*!**********************************************************************
+		 * \copydoc implemented_element::implemented_element
+		 ************************************************************************/
+		pseudo_element (grids::axis i_axis_n, grids::axis i_axis_m, int i_name, io::parameters& i_params, data::data <datatype> &i_data, mpi::messenger* i_messenger_ptr, int i_element_flags, bool load_diffusion = true);
+		
+		virtual ~pseudo_element () {}
+	};
+} /* pisces */
+
+#endif /* end of include guard: pseudo_TWO_D_HPP_OQ800X4X */
