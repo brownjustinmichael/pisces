d752c96b0cfe12e03a9f1e9712ca27e55d8d1eec
Justin Brown <brown.justin.michael@gmail.com>
2016-01-20 16:26:19 -0800

    Easier interface for hyades

--- run/run.py
+++ run/run.py
@@ -1,9 +1,19 @@
 import os
 import argparse
+import yaml
 
 import pisces_utils.config as config
 import pisces_utils.launch as launch
 import pisces_utils.torque_launch as tlaunch
+
+exports = {}
+try:
+	with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "run_custom.yaml", 'r') as stream:
+		new_exports = yaml.load(stream)
+		for key in new_exports:
+		    exports [key] = new_exports [key]
+except IOError:
+	pass
 
 parser = argparse.ArgumentParser()
 
@@ -12,15 +22,16 @@ parser.add_argument("--np", type=int, default=1)
 parser.add_argument("--init", default=None)
 parser.add_argument("--code", default="ISCES")
 parser.add_argument("--launcher", default="Launcher")
-parser.add_argument("--mpi", default="mpirun")
 
 args = parser.parse_args()
 
 configuration = config.Configuration(args.config_file)
 configuration["np"] = args.np
 
-code = launch.CodeRegistry.registry[args.code](configuration, init=args.init)
+code = launch.CodeRegistry.registry[args.code](configuration, init=args.init, )
+
+exports ["init"] = (args.init not in ["False", "false", "f", "F"])
 
 launcher = launch.LauncherRegistry.registry[args.launcher](code)
-launcher.launch(init=(args.init not in ["False", "false", "f", "F"]), mpi=args.mpi)
+launcher.launch(**exports)
 launcher.wait()
